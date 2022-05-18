04f33842214c38b9d6a4b639adc5a94521a396b3
usma11dia0 <usma11dia0@gmail.com>
2022-05-19 04:54:57 +0900

    create router

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { Button, ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from 'react-router-dom';
 
 import theme from './theme/theme';
+import { Router } from './router/Router';
 
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <BrowserRouter>
-        <Button colorScheme="teal">ボタン</Button>
+        <Router></Router>
       </BrowserRouter>
     </ChakraProvider>
   );
